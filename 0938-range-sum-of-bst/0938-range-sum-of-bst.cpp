fab20f641b7092c17d0a8842d3b25e9bae8cd389
Mradul gupta <mradulgupta387a@gmail.com>
2026-07-14 12:53:50 +0530

    Time: 1 ms (17.02%), Space: 65 MB (60.76%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -1,35 +1,15 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    vector<int>inorder(TreeNode* root){
-        vector<int>vec;
-        if(root==NULL) return vec;
-        vector<int>leftv=inorder(root->left);
-        vec.insert(vec.end(),leftv.begin(),leftv.end());
-        vec.push_back(root->val);
-        vector<int>rightv=inorder(root->right);
-        vec.insert(vec.end(),rightv.begin(),rightv.end());
-        return vec;
-    }
     int rangeSumBST(TreeNode* root, int low, int high) {
-        vector<int>vec=inorder(root);
-        int sum = 0 ;
-        for(int i = 0 ;i<vec.size();i++){
-            if(vec[i]>=low and vec[i]<=high){
-                sum+=vec[i];
-            }
+        if (!root) {
+            return 0;
         }
-        return sum;
         
+        int currentVal = (root->val >= low && root->val <= high) ? root->val : 0;
+        
+        int leftSum = rangeSumBST(root->left, low, high);
+        int rightSum = rangeSumBST(root->right, low, high);
+        
+        return currentVal + leftSum + rightSum;
     }
 };
