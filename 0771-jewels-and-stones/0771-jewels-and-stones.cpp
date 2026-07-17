34b7eb1a2d5aff638eee7b57231c6650161c6bf2
Mradul gupta <mradulgupta387a@gmail.com>
2026-07-17 11:07:49 +0530

    Time: 3 ms (11.57%), Space: 8.9 MB (7.52%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_set<char> m(jewels.begin(),jewels.end());
+        int count=0;
+        for(int i = 0 ;i<stones.size();i++){
+            if(m.contains(stones[i])){
+            count++;
+            }
+        }
+        return count;
+    }
+};
