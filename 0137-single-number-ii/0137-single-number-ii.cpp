7e4f9a6971a0f8ec2c3d217a08c670f1ee78879e
Mradul gupta <mradulgupta387a@gmail.com>
2026-07-26 16:50:16 +0530

    Time: 4 ms (14.15%), Space: 14.1 MB (11.05%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int>m;
+        for(int i =0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        for(auto&[num,count]:m){
+            if(count==1) return num;
+
+        }
+        return -1;
+    }
+};
