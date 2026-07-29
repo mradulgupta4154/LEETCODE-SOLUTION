9ebac0dd8c2fc8355ab024ac1f9e005d044915fa
Mradul gupta <mradulgupta387a@gmail.com>
2026-07-29 14:42:19 +0530

    Time: 0 ms (100%), Space: 8.8 MB (91.9%) - LeetHub

---
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstMatchingIndex(string s) {
+        int l=0;
+        int r=s.length()-1;
+        while(l<=r){
+            if(s[l]==s[r]){
+                return l;
+                break;
+            }
+            else {
+                r--;
+                l++;
+            }
+        }
+        return -1;
+        
+    }
+};
