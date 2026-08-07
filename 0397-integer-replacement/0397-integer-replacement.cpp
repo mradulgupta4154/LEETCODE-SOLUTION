7b19776e68c5e3bce5853c2370d369100249e5e8
Mradul gupta <mradulgupta387a@gmail.com>
2026-08-07 16:15:02 +0530

    Time: 0 ms (100%), Space: 7.7 MB (97.83%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int integerReplacement(int n) {
+        if (n == INT_MAX) return 32; 
+        int count=0;
+        while(n>1){
+            if(n%2==0){
+                n/=2;
+            }
+            else {
+                if((n+1)%4==0 and (n-1!=2)) n++;
+                else n--;
+            }
+            count++;
+        }
+        return count;
+    }
+};
