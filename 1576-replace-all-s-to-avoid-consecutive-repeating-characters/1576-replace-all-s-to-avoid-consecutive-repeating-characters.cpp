5de11ce8e4693ae3dba3ca9e5cc4ceb6713d5cc2
Mradul gupta <mradulgupta387a@gmail.com>
2026-08-18 13:07:57 +0530

    Time: 0 ms (100%), Space: 8.5 MB (16.23%) - LeetHub

---
+++ 1576-replace-all-s-to-avoid-consecutive-repeating-characters/1576-replace-all-s-to-avoid-consecutive-repeating-characters.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string modifyString(string s) {
+    for (auto i = 0; i < s.size(); ++i)
+        if (s[i] == '?')
+            for (s[i] = 'a'; s[i] <= 'c'; ++s[i])
+                if ((i == 0 || s[i - 1] != s[i]) && (i == s.size() - 1 || s[i + 1] != s[i]))
+                    break;
+    return s;
+}
+};
