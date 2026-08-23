80383ae4f2b91b80dcdba9db2f26113d03f775d9
Mradul gupta <mradulgupta387a@gmail.com>
2026-08-23 11:50:41 +0530

    Time: 0 ms (100%), Space: 8 MB (95.66%) - LeetHub

---
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+    double a = abs(30.0*hour - 5.5*minutes);
+    if (a >= 180) {
+        return 360 - a;
+    }
+    return a;
+        
+    }
+};
