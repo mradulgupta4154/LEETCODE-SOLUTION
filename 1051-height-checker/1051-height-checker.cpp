f3cc1085eda3cf5bddaa62cd1cf01f4012df6ace
Mradul gupta <mradulgupta387a@gmail.com>
2026-08-26 11:29:28 +0530

    Time: 0 ms (100%), Space: 11.4 MB (32.91%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int>vec=heights;
+        sort(vec.begin(),vec.end());
+        int count=0;
+        for(int i=0;i<vec.size();i++){
+            if(vec[i]!=heights[i]) count++;
+        }
+        return count;
+        
+    }
+};
