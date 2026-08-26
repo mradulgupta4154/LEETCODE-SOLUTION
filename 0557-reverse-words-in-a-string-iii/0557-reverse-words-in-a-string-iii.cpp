a772d81e6736972f49c3b4887d79ee8ead15a0a5
Mradul gupta <mradulgupta387a@gmail.com>
2026-08-26 11:26:53 +0530

    Time: 0 ms (100%), Space: 12.1 MB (88.45%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n =s.length();
+        int i=0;
+        while(i<n){
+            int left=i;
+            while(i<n && s[i] != ' '){
+                i++;
+            }
+            int right = i-1;
+            while(left < right){
+                swap(s[left], s[right]);
+                left++;
+                right--;
+            }
+            i++;
+        }
+        return s;
+    }
+};
